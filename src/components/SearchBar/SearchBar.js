982a07715289026dbbf8173137af50d2bfcb9747
sagarpoojary <poojarysagar09@gmail.com>
2020-06-23 21:42:16 +0530

    Add user feedback

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,33 +1,77 @@
 import React from 'react';
 import './SearchBar.css';
 
-const sortByOptions =    {
-    'Best Match': 'best_match',
-    'Highest Rated': 'rating',
-    'Most Reviewed': 'review_count'
-};
 
 class SearchBar extends React.Component {
+
+    constructor(props)  {
+        super(props);
+        this.state = {
+            term : '',
+            location: '',
+            sortBy: 'best_match'
+        };
+        this.sortByOptions =    {
+            'Best Match': 'best_match',
+            'Highest Rated': 'rating',
+            'Most Reviewed': 'review_count'
+        };
+
+        this.handleTermChange = this.handleTermChange.bind(this);
+        this.handleLocationChange = this.handleLocationChange.bind(this);
+        this.handleSearch = this.handleSearch.bind(this);
+
+    }
+
+    getSortByClass(sortByOption)    {
+        if (this.state.sortBy === sortByOption)  {
+            return 'active';
+        }
+        return '';
+    }
+
+    handleSortByChange(sortByOption)    {
+        this.setState({sortBy: sortByOption});
+    }
+
+    handleTermChange(event)  {
+        this.setState({term: event.target.value});
+    }
+
+    handleLocationChange(event)  {
+        this.setState({location: event.target.value});
+    }
+
+    handleSearch(event) {
+        this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy);
+        event.preventDefault();
+    }
+
+
     renderSortByOptions()   {
-        return Object.keys(sortByOptions).map(sortByOption => {
-            let sortByOptionValue = sortByOptions[sortByOption];
-        return <li key={sortByOptionValue}> {sortByOption} </li>;
+        return Object.keys(this.sortByOptions).map(sortByOption => {
+            let sortByOptionValue = this.sortByOptions[sortByOption];
+            /* 
+                Passing in this in bind will allow us to both bind to the current value of this (as we usually do in the constructor()) but 
+                also bind the current sortByOptionValue as the first argument to the method call, ensuring the method is called with the appropriate value when clicked.
+            */
+        return <li className={this.getSortByClass(sortByOptionValue)} key={sortByOptionValue} onClick={ this.handleSortByChange.bind(this, sortByOptionValue) } > {sortByOption} </li>;
         });
     }
 
     render()    {
         return (
-            <div className="SearchBar">
+            <div className="SearchBar" >
                 <div className="SearchBar-sort-options">
                     <ul>
-                        {this.renderSortByOptions}
+                        {this.renderSortByOptions()}
                     </ul>
                 </div>
                 <div className="SearchBar-fields">
-                    <input placeholder="Search Businesses" />
-                    <input placeholder="Where?" />
+                    <input placeholder="Search Businesses" onChange={this.handleTermChange} />
+                    <input placeholder="Where?" onChange={this.handleLocationChange} />
                 </div>
-                <div className="SearchBar-submit">
+                <div className="SearchBar-submit" onClick={this.handleSearch} >
                     <a>Let's Go</a>
                 </div>
             </div>
