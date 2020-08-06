0b3d7651f308b64e92a1bad0d4770129c01dddab
sagarpoojary <poojarysagar09@gmail.com>
2020-08-07 01:15:15 +0530

    Added env

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,36 +1,37 @@
-const apiKey = 'ADD_YOUR_API_KEY';
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 const yelp = {
-    search(term, location, sortBy)  {
-        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
-            headers: {
-                Authorization: `Bearer ${apiKey}`
-            }
-        }).then(response => {
-            return response.json();
-        }).then(jsonResponse => {
-            if (jsonResponse.businesses)    {
-                return jsonResponse.businesses.map(business => {
-                    return {
-                        id: business.id,
-                        imageSrc: business.image_url,
-                        name: business.name,
-                        address: business.location.address1,
-                        city: business.location.city,
-                        state: business.location.state,
-                        zipCode: business.location.zip_code,
-                        category: business.categories[0].title,
-                        rating: business.rating,
-                        reviewCount: business.review_count,
-
-                    }
-                });
-            }
-        })
-    }
-
+  search(term, location, sortBy) {
+    return fetch(
+      `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+      {
+        headers: {
+          Authorization: `Bearer ${API_KEY}`,
+        },
+      }
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((jsonResponse) => {
+        if (jsonResponse.businesses) {
+          return jsonResponse.businesses.map((business) => {
+            return {
+              id: business.id,
+              imageSrc: business.image_url,
+              name: business.name,
+              address: business.location.address1,
+              city: business.location.city,
+              state: business.location.state,
+              zipCode: business.location.zip_code,
+              category: business.categories[0].title,
+              rating: business.rating,
+              reviewCount: business.review_count,
+            };
+          });
+        }
+      });
+  },
 };
 
 export default yelp;
-
-
